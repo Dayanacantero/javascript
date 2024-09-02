3026f8a9e1a8db57e13f7302964ca987b57f1bff
unknown <astridcantero2005@gmail.com>
2024-09-02 01:37:54 -0500

    subir

---
+++ javascript.js
@@ -0,0 +1,42 @@
+//let nombre = "Juan";
+//console.log("¡Hola, " + nombre + "!");
+
+
+//function calcularAreaTriangulo(base, altura) {
+  //  return (base * altura) / 2;
+//}
+
+
+//let area = calcularAreaTriangulo(5, 10);
+//console.log("El área del triángulo es: " + area);
+
+
+
+//let numero = -3;
+//if (numero > 0) {
+  //  console.log("El número es positivo.");
+//} else if (numero < 0) {
+  //  console.log("El número es negativo.");
+//} else {
+//    console.log("El número es cero.");
+//}
+//for (let i = 1; i <= 10; i++) {
+  //  console.log(i);
+//}
+
+
+//let boton = document.querySelector("button");
+//boton.textContent = "Nuevo Texto del Botón";
+
+
+//let elemento = document.querySelector("div");
+//elemento.addEventListener("click", function() {
+  //  elemento.style.backgroundColor = "blue";
+//});
+
+
+//let boton = document.querySelector("button");
+//boton.addEventListener("click", function() {
+  //  alert("¡Botón clicado!");
+//});
+
